5bb77dd22ced3410296954563166227d88db9945
Ben Fu <ben.fu@verses.io>
2022-11-01 10:56:16 -0500

    Add safe dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 ls -al
 env
 
+cd "${GITHUB_WORKSPACE}" || exit
+
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@github.verses.io"
 
