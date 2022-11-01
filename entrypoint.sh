1d042560bd127083adbed889e3cf14ffc05722e3
Ben Fu <ben.fu@verses.io>
2022-11-01 11:02:58 -0500

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@github.verses.io"
 
-release-it --ci --config ./.release-it.json --dry-run 
+release-it --ci --config ./.release-it.json -VV
